fdbb444b13eeba4c4f7fcf49829dae02af9fe0fa
noureddine.bensadok@ouidou.fr <noureddine.bensadok@ouidou.fr>
2024-10-30 21:56:34 +0100

    project structure + mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { mongooseConnect } from "./middleware/mongoDBconnect";
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+mongooseConnect();
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
